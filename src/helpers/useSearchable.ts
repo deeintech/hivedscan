9b5f0dcb9cddcf4ef1a672cfb085b9320eb10ead
dmitrydao <dmitrydaopro@gmail.com>
2020-10-11 14:40:05 +0300

    [Update]: search and input hooks

--- src/helpers/useSearchable.ts
+++ src/helpers/useSearchable.ts
@@ -7,19 +7,4 @@ const useSearchable = <T>(data: T[], searchText: string, searchProps: (item: T) 
     );
   }, [data, searchText, searchProps]);
 };
-
 export default useSearchable;
-
-// const useSearchableDuo = <T>(data1: T[], data2: T[], searchText: string, searchProps: (item: T) => string[]) => {
-//   return useMemo(() => {
-//     const regex = new RegExp(searchText, "i");
-//     data1.filter((item) =>
-//       searchProps(item).some((sp) => regex.test(sp))
-//     );
-//     data2.filter((item) =>
-//       searchProps(item).some((sp) => regex.test(sp))
-//     );
-//     return { data1, data2 };
-//   }, [data1, data2, searchText, searchProps]);
-// // };
-// export default { useSearchable, useSearchableDuo };
--- src/helpers/useUserInput.ts
+++ src/helpers/useUserInput.ts
@@ -2,7 +2,6 @@ import { useState, useCallback } from "react";
 const useUserInput = (defaultValue: string = "") => {
   const [value, setValue] = useState(defaultValue);
   const onChange = useCallback((e) => setValue(e.target.value), []);
-  console.log(value);
   return {value, onChange,};
 };
 export default useUserInput;
