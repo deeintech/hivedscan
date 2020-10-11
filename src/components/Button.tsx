0955ba0cf402d6811dce8a0137315506057d4646
dmitrydao <dmitrydaopro@gmail.com>
2020-10-12 00:33:15 +0300

    [Update]: button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,13 +4,17 @@ import { Button } from "reactstrap";
 type Props = {
   style: string,
   textStyle: string,
-  content: string
+  content: string,
+  icon: string
 }
 
-export const AppButton = ({ style, textStyle, content }: Props) => {
+export const AppButton = ({ style, textStyle, content, icon }: Props) => {
   return (
-    <Button className={`btn-icon btn-2 ${textStyle}`} color={style} type="button">
-      {content}
+    <Button className={`btn-icon btn-3 ${textStyle}`} color={style} type="button" >
+      <span className="btn-inner--icon">
+        <i className={icon} />
+      </span>
+      <span className="btn-inner--text">{content}</span>
     </Button>
   );
 }
