d5c112cee0660e20fc3932068378d7ddaffffbd7
dmitrydao <dmitrydaopro@gmail.com>
2020-10-20 00:03:11 +0300

    [Add]: info details page

---
+++ src/features/info/InfoDetailsPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const InfoDetailsPage = () => {
+  
+
+  let renderInfo = (
+    <div>
+      This is an informational page.
+    </div>
+  );
+
+  return (
+    <>
+      {renderInfo}
+    </>
+  );
+}
