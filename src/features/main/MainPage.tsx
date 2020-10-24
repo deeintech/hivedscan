dca6416661c87133806db1d0fb313a69a4f6f47e
dmitrydao <dmitrydaopro@gmail.com>
2020-10-25 00:00:27 +0300

    [Add]: main page

---
+++ src/features/main/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const MainPage = () => {
+  
+
+  let renderMain = (
+    <div>
+      This is a main page.
+    </div>
+  );
+
+  return (
+    <>
+      {renderMain}
+    </>
+  );
+}
