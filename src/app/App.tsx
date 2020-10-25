422076aa9ca631132a7f9d90424a5e3e73e88f8d
dmitrydao <dmitrydaopro@gmail.com>
2020-10-25 19:02:08 +0300

    [Update]: app structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Container } from "reactstrap";
 import { Route } from "react-router-dom";
 import { ProposalDetailsPage } from 'features/proposals/proposalDetails/ProposalDetailsPage';
-import { InfoDetailsPage } from 'features/info/InfoDetailsPage';
 import { MainPage } from 'features/main/MainPage';
 import AppNavbar from 'components/Nav';
 
@@ -12,20 +11,19 @@ function App() {
   return (
     <div>
       <AppNavbar />
+      <Route path="/" exact={true} render={() => (
+        <MainPage />
+      )} />
       <Route path="/fund" exact={true} render={() => (
         <AppHeader
-          title="Decentralized Hive"
+          title="Decentralized Hive Fund"
           subtitle="A list of the latest proposals submitted to the Hive network" />
       )} />
       <Container>
-        <Route path="/" exact={true} render={() => (
-          <MainPage />
-        )} />
         <Route path="/fund" exact={true} render={() => (
           <ProposalsListPage />
         )} />
         <Route path={`/proposal/:id`} component={ProposalDetailsPage} />
-        <Route path={"/info"} exact={true} component={InfoDetailsPage} />
       </Container>
     </div>
   );
