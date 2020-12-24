5cb25f5174c3b12dff566b89cefc5836b615d709
dmitrydao <dmitrydaopro@gmail.com>
2020-12-24 15:08:36 +0200

    [Add]: hive engine config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 const config = {
   hiveConfig: process.env.REACT_APP_HIVE_MAINNET,
+  hiveEngineRpcConfig: process.env.REACT_APP_HIVE_ENGINE_RPC,
+  hiveEngineAccountsConfig: process.env.REACT_APP_HIVE_ENGINE_ACCOUNTS,
   hdfAccount: "hive.fund",
   returnProposalAccount: "steem.dao",
   returnProposalId: 0
