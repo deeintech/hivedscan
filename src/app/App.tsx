f2cfd8fb2ad46bc5bc5ae01919dad3ad824c0dd8
dmitrydao <dmitrydaopro@gmail.com>
2020-12-24 15:08:59 +0200

    [Update[: navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,21 +6,39 @@ import { Route } from "react-router-dom";
 import { ProposalDetailsPage } from 'features/proposals/proposalDetails/ProposalDetailsPage';
 import AppNavbar from 'components/Nav';
 import AppFooter from 'components/Footer';
+import { TransactionsListPage } from 'features/hive-engine/transactionsList/TransactionsListPage';
+import { MainPage } from 'features/main/MainPage';
 
 function App() {
   return (
     <div>
       <AppNavbar />
-      <Route path="/" exact={true} render={() => (
+      {/* <Route path="/" exact={true} render={() => (
         <AppHeader
           title="Decentralized Hive Fund"
           subtitle="A list of the latest proposals submitted to the Hive network" />
+      )} /> */}
+      <Route path="/fund" exact={true} render={() => (
+        <AppHeader
+          title="Decentralized Hive Fund"
+          subtitle="The latest proposals submitted to the Hive network" />
+      )} />
+      <Route path="/hive-engine" exact={true} render={() => (
+        <AppHeader
+          title="Hive Engine Explorer"
+          subtitle="The latest smart contracts transactions on Hive" />
       )} />
       <Container>
-        <Route path="/" exact={true} render={() => (
+      <Route path="/" exact={true} render={() => (
+          <MainPage />
+        )} />
+        <Route path="/fund" exact={true} render={() => (
           <ProposalsListPage />
         )} />
         <Route path={`/proposal/:id`} component={ProposalDetailsPage} />
+        <Route path="/hive-engine" exact={true} render={() => (
+          <TransactionsListPage />
+        )} />
       </Container>
       <AppFooter />
     </div>
